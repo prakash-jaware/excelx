a56c79e9fe3a63db1cc928a69057fcf8886358d2
prakash-jaware <114635043+prakash-jaware@users.noreply.github.com>
2025-04-07 22:35:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import tempfile
 
 app = Flask(__name__)
 UPLOAD_FOLDER = tempfile.gettempdir()
-UPLOAD_FOLDER = Path("./Documents/")
+UPLOAD_FOLDER = Path("C:/Users/ASUS/Documents/")
 OUTPUT_FOLDER.mkdir(parents=True, exist_ok=True)
 
 @app.route("/", methods=["GET", "POST"])
